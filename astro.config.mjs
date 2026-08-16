c851f3daa6b7fe31525b7672f936651dd719349c
Ujonathan6 <167881778+Ujonathan6@users.noreply.github.com>
2026-08-16 02:03:36 -0500

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://Ujonathan6.github.io',
   integrations: [mdx(), sitemap()],
 
   vite: {
